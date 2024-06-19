cb2eb211d082e96e20e662d7d6f138f51655136b
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-06-19 14:37:44 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let student ={
+	name:"hamza",
+}
+
+ Object.prototype.getkeys= function(){
+     return Object.keys(this)
+ }
+
+ console.log(student.getkeys())
